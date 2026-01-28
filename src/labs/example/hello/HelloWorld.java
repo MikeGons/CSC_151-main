24fef9b735e565e63bf7f79ac359ac832e104026
SANDHILLS\lh218-03 <jad.talbert@gamil.com>
2026-01-28 11:13:19 -0500

    Hello World Changes!

---
+++ src/labs/example/hello/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+Author: Michael Gonsalves
+Date: 1/21/2026
+Reason: First Java File
+*/
+
+package labs.example.hello;
+
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
