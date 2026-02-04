2ac62cea5c3106d4a9603ecfaabc25b2471fc493
SANDHILLS\lh218-03 <jad.talbert@gamil.com>
2026-02-04 11:32:26 -0500

    animals

---
+++ src/labs/example/mammals/Main.java
@@ -0,0 +1,18 @@
+package labs.example.mammals;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Mammal mammal = new Mammal();
+
+        /*mammal.setName("Dog");
+        String myMammalName = mammal.getName();
+        System.out.println(myMammalName);
+        */
+
+
+
+        mammal.eat();
+        /*mammal.sleep();*/
+        mammal.sit();
+    }
+}
--- src/labs/example/mammals/Mammal.java
+++ src/labs/example/mammals/Mammal.java
@@ -2,15 +2,26 @@
     Date: 1/28/2025
     Purpose: Constructing a class that has several methods and properties relating to mammals.
 */
+package labs.example.mammals;
+public class Mammal {
 
 
 
-private String hairColor;
-private String eyeColor;
-private double bodyTemp;
-private int height;
-private int weight;
-private int species;
+private String hairColor = "Black";
+private String eyeColor = "Blue";
+private double bodyTemp = 89;
+private String height = "1 ft";
+private String weight = "26 lb" ;
+private String species = "Lab";
+
+public void getMammalDetails(){
+    System.out.println(this.hairColor);
+    System.out.println(this.eyeColor);
+    System.out.println(this.bodyTemp);
+    System.out.println(this.height);
+    System.out.println(this.weight);
+    System.out.println(this.species);
+}
 
 
 public void scratch(){
@@ -29,11 +40,11 @@ public void drink(){
 
 }
 
-public void sleep() throws Exception{
+/*public void sleep() throws Exception{
     int sleepTime = 5000;
-    System.out.println("The " + this.name + " is sleeping and will wake up in" sleepTime/1000 " seconds") 
+    System.out.println("The " + this.name + " is sleeping and will wake up in " sleepTime/1000 " seconds");
 }
-
+*/
 public void jump(){
 
 }
@@ -42,6 +53,15 @@ public void fight(){
 
 }
 
+/*public void setName(String name){
+    this.name = name;
+    return;
+}
+
+public String getName(){
+    return this.name;
+}
+*/
 public void swim(){
 
 }
@@ -51,9 +71,14 @@ public void walk(){
 }
 
 public void sit() throws Exception{
-    int sitTime = 15000;
+    Thread.sleep(15000);
+    System.out.println("The mammal is no longer sitting");
+    Thread.sleep(5000);
+    stand();
+}
+public void stand(){
+    System.out.println("The mammal is now up and barking");
 }
 
-public void getMammalDetails(){
 
 }
