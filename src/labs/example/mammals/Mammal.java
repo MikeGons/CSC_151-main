ea7091256f37cfd9a7c45ed25832f28b250b517f
SANDHILLS\lh218-03 <jad.talbert@gamil.com>
2026-02-04 10:34:32 -0500

    here we gooooooooooo!

---
+++ src/labs/example/mammals/Mammal.java
@@ -0,0 +1,59 @@
+/* Name: Michael Gonsalves
+    Date: 1/28/2025
+    Purpose: Constructing a class that has several methods and properties relating to mammals.
+*/
+
+
+
+private String hairColor;
+private String eyeColor;
+private double bodyTemp;
+private int height;
+private int weight;
+private int species;
+
+
+public void scratch(){
+
+}
+
+public void run(){
+
+}
+
+public void eat(){
+
+}
+
+public void drink(){
+
+}
+
+public void sleep() throws Exception{
+    int sleepTime = 5000;
+    System.out.println("The " + this.name + " is sleeping and will wake up in" sleepTime/1000 " seconds") 
+}
+
+public void jump(){
+
+}
+
+public void fight(){
+
+}
+
+public void swim(){
+
+}
+
+public void walk(){
+
+}
+
+public void sit() throws Exception{
+    int sitTime = 15000;
+}
+
+public void getMammalDetails(){
+
+}
